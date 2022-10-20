9df0be7a48ff8063a6ae6ece81220927ecf7e841
Minh Quan Do <minhquan98vn@gmail.com>
2022-10-20 16:08:58 +0700

    hoan thanh session 3

--- session3/src/App.js
+++ session3/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const data = [
+  "Clean up bedroom",
+  "Buy some milk",
+  "Jogging",
+  "Learn React",
+  "Doing exercise",
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input id="add_todo" type="text" placeholder="Enter your task here..." />
+      <div className="list">
+        {data.map((elem, index) => {
+          const attr = elem.split(" ").join("_").toLowerCase();
+          return (
+            <div className="todo" key={index}>
+              <input
+                className="checkbox"
+                type="checkbox"
+                name={attr}
+                id={attr}
+                value={index}
+              />
+              <label htmlFor={attr}>{elem}</label>
+            </div>
+          );
+        })}
+      </div>
+      <footer>
+        <span>{data.length} task(s) left!</span>
+        <span>MindX todolist</span>
+      </footer>
     </div>
   );
 }
